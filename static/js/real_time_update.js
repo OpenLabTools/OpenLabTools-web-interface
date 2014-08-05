59d1eaa424a8a49cc576a5672ec8e9655676f47d
Leo Zhou <leothemagnificent@gmail.com>
2014-08-05 17:39:26 +0100

    Enabled large Text display of real time data

--- static/js/real_time_update.js
+++ static/js/real_time_update.js
@@ -56,3 +56,16 @@ function button_ajax(id, status_indicator, extra_args) {
         btn.button('reset');
     });
 }
+
+// return a formated string representation of a float with fiex digits of precision
+function toFixed(value, precision) {
+    var precision = precision || 0,
+    neg = value < 0,
+    power = Math.pow(10, precision),
+    value = Math.round(value * power),
+    integral = String((neg ? Math.ceil : Math.floor)(value / power)),
+    fraction = String((neg ? -value : value) % power),
+    padding = new Array(Math.max(precision - fraction.length, 0) + 1).join('0');
+
+    return precision ? integral + '.' +  padding + fraction : integral;
+}
