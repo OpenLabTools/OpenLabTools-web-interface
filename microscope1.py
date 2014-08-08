e16f02afd1759dfa09476863a146026099fb554d
Leo Zhou <leothemagnificent@gmail.com>
2014-08-08 12:31:40 +0100

    Fix error: cannot sent int to a serial port. String required. Hard coded default arduino ttyAMC0 port

--- microscope1.py
+++ microscope1.py
@@ -6,21 +6,22 @@ class Microscope():
         self.temp = 1
         self.set_temp = self.temp
         self.control_temp = True
-        self.ser = serial.Serial(0)
+        port = "/dev/ttyACM0"
+        self.ser = serial.Serial(port)
         #possibly change the above line to be relevant to the arduino serial port
 
     def __del__(self):
         self.ser.close();
-        #destructing the class, closing down 
-        #the serial connection to avoid things getting bad 
+        #destructing the class, closing down
+        #the serial connection to avoid things getting bad
 
     def motion_control(self, direction="Down"):
         return direction
 
     def get_temp(self):
-        self.ser.write(1)#tells the arduino to the read the temperature
+        self.ser.write("1")#tells the arduino to the read the temperature
         self.temp=self.ser.readline()
-        return self.temp
+        return float(self.temp)
 
     def set_set_temp(self, temp):
         self.set_temp = float(temp)
@@ -33,6 +34,6 @@ class Microscope():
         self.control_temp = not self.control_temp
         return self.control_temp
 
-    
+
 if __name__ == "__main__":
     r = Microscope()
