c14c538920782c82a094ccf8822f9d44512d3b50
Leo Zhou <leothemagnificent@gmail.com>
2014-08-11 10:32:16 +0100

    Modified calling method for PiServer load the dummy when arduino not exist.

--- PiServer.py
+++ PiServer.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
-from Microscope_dummy import Microscope
+import serial
+
 
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
-# Create server
-server = SimpleXMLRPCServer(("localhost", 8000),
-                            requestHandler=RequestHandler)
-server.register_introspection_functions()
 
-# Register an instance; all the methods of the instance are
-# published as XML-RPC methods
-server.register_instance( Microscope() )
+if __name__ == "__main__":
+    try:
+        serial.Serial('/dev/ttyACM0').close()
+        from microscope1 import Microscope
+    except:
+        from Microscope_dummy import Microscope
 
-# Run the server's main loop
-server.serve_forever()
+    # Create server
+    server = SimpleXMLRPCServer(("localhost", 8000), requestHandler=RequestHandler)
+    server.register_introspection_functions()
+
+    # Register an instance; all the methods of the instance are
+    # published as XML-RPC methods
+    server.register_instance( Microscope() )
+
+    # Run the server's main loop
+    server.serve_forever()
