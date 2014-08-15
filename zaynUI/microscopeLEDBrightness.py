f8ebb39032f253a4a3ba3ac38b552e00f955897e
ZaynHanif <zayn_hanif@hotmail.co.uk>
2014-08-15 12:01:25 +0100

    Adding get and LED test functions

--- zaynUI/microscopeLEDBrightness.py
+++ zaynUI/microscopeLEDBrightness.py
@@ -8,21 +8,21 @@ class Microscope():
         self.ser = serial.Serial(port, 9600, timeout=3)
         #possibly change the above line to be relevant to the arduino serial port
 
-
     def __del__(self):
         self.ser.close();
         #destructing the class, closing down
         #the serial connection to avoid things getting bad
-
-
-    def get_brightness(self):
-        self.ser.write( "1\n" )#tells the arduino to the read the temperature
-        self.temp=self.ser.readline()
-        if self.temp == '': self.temp = 1
-        return float(self.temp)
-
-
+   
     def set_brightness(self, brightness):
         print "set_brightness"
-        self.ser.write(str(int(float(brightness))) + "\n")
-        return self.brightness
+        self.brightness = brightness
+        self.ser.write("1 " + str(int(float(self.brightness))) + "\n")
+
+    def get_brightness(self):
+        self.ser.write( "2\n" )#tells the arduino to the get the current brightness level
+        self.brightness=self.ser.readline()
+        return int(self.brightness)
+
+	def test_LED(self):
+		print "Testing LED"
+		self.ser.write("3\n")
