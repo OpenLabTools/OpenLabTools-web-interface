3e797d49b938c925699fd37e8cda2aed744bbe14
Liyou Zhou <leothemagnificent@gmail.com>
2014-11-11 23:45:31 +0000

    Automatic add ID function back in

--- code/OLT_config_parser.py
+++ code/OLT_config_parser.py
@@ -6,7 +6,10 @@ import re
 id_len = 8
 
 def get_config(config_fn):
-    return parse_config(config_fn)
+    config_dict = parse_config(config_fn)
+    changed = add_id(config_dict)
+    if changed: write_config(config_fn, config_dict)
+    return config_dict
 
 def get_config_by_id(config_dict, elem_id):
     for section_name, section in config_dict.iteritems():
@@ -17,6 +20,18 @@ def get_config_by_id(config_dict, elem_id):
                 return subsec.update({'name': subsec_name})
     return None
 
+def add_id(config_dict):
+    change_flag = False
+    for section_name, section in config_dict.iteritems():
+        if 'id' not in section.keys():
+            change_flag = True
+            section.update({'id': str(uuid4())[:id_len]})
+        for subsec_name, subsec in section.iteritems():
+            if subsec_name != 'id' and 'id' not in subsec.keys():
+                change_flag = True
+                subsec.update({'id': str(uuid4())[:id_len]})
+    return change_flag
+
 def print_config( config_dict ):
     s = ''
     for section_name, section in config_dict.iteritems():
@@ -26,7 +41,8 @@ def print_config( config_dict ):
             if subsec_id != 'id':
                 s = s + '    [[%s]]\n'%subsec_id
                 for key, value in subsec.iteritems():
-                    s = s + '         %s = %s\n'%(key, value)
+                    s = s + '        %s = %s\n'%(key, value)
+        s = s + '\n'
     return s
 
 def write_config( config_fn, config_dict ):
@@ -59,7 +75,7 @@ def parse_config( config_fn ):
     config_d = OrderedDict()
     with open( config_fn, 'r' ) as fn:
         config_str = fn.read()
-        print config_str
+        config_str = re.sub(r'#.+', '', config_str, flags=re.MULTILINE)
         pattern = r'^\[[^\[]+]'
         m = re.split( pattern,
                       config_str,
@@ -79,11 +95,11 @@ if __name__ == '__main__':
 
     config_fn = 'E:\OpenLabTools-web-interface\code\examples\WidgetOverview\UI_config.ini'
     #config_fn = 'examples\OLT_cluster_config.ini'
-    config1 = parse_config( config_fn )
+    config1 = get_config( config_fn )
     fileTemp = tempfile.NamedTemporaryFile(delete = False)
     write_config( fileTemp.name, config1 )
     config2 = parse_config( fileTemp.name )
-    print print_config( config2 )
+    #print print_config( config2 )
     assert(config1 == config2)
     fileTemp.close()
     os.remove(fileTemp.name)
